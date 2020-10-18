556d894819e99b482f2f07881a03e4431fefc6f8
wuling31715 <wuling31715@gmail.com>
2020-10-19 00:00:09 +0800

    .

--- lab1/helpers/data_mining_helpers.py
+++ lab1/helpers/data_mining_helpers.py
@@ -1,4 +1,6 @@
 import nltk
+import pandas as pd
+import matplotlib.pyplot as plt
 
 """
 Helper functions for data mining lab session 2018 Fall Semester
@@ -36,3 +38,20 @@ def tokenize_text(text, remove_stopwords=False):
             # filters here
             tokens.append(word)
     return tokens
+
+def plot_term_matrix(matrix, precision):
+    plt.subplots(figsize=(20, 25))
+    plt.spy(matrix, precision=precision, markersize=1)
+
+def convert_file_to_dataframe(file):
+    with open(file, 'r', encoding='utf8') as reader:
+        lines = reader.readlines()
+        sentence = list()
+        label = list()
+        for l in lines:
+            split = l.split('\t')
+            sentence.append(split[0])
+            label.append(split[1].rstrip())
+        df = pd.DataFrame({'sentence': sentence, 'label': label})
+
+    return df
